a84634c7bab08d581a8927931109e2597dd6abc1
よしひさ <yosihisa.n@gmail.com>
2024-07-28 16:53:18 +0900

    同期用信号出力に対応

--- src/LoadCell-Logger/LoadCell-Logger.ino
+++ src/LoadCell-Logger/LoadCell-Logger.ino
@@ -17,38 +17,40 @@
 //#define LOAD      500.0f    //定格容量 [g]
 
 
-#define HX711_AVDD 4.000f // ( 1.25*(( 22k + 10k )/ 10 ) )
+#define HX711_AVDD 4.000f  // ( 1.25*(( 22k + 10k )/ 10 ) )
 #define HX711_ADC1bit ((HX711_AVDD / 16777216))
 #define HX711_SCALE (OUT_VOL * HX711_AVDD / LOAD * 128)
 
 #define OFFSET_AVERAGING 100
 
 //----------ピン定義----------
-#define CH1_DATA  14
-#define CH1_SCK   15
-#define CH2_DATA  12
-#define CH2_SCK   13
-#define CH3_DATA  10
-#define CH3_SCK   11
-#define CH4_DATA  8
-#define CH4_SCK   9
-#define LED1      18
-#define LED2      19
-#define LED3      20
-#define LED4      21
-#define SW_A      22
-#define SW_B      26
+#define CH1_DATA 14
+#define CH1_SCK 15
+#define CH2_DATA 12
+#define CH2_SCK 13
+#define CH3_DATA 10
+#define CH3_SCK 11
+#define CH4_DATA 8
+#define CH4_SCK 9
+#define LED1 18
+#define LED2 19
+#define LED3 20
+#define LED4 21
+#define SW_A 22
+#define SW_B 26
+#define PIN_TX 0
 //----------ピン定義----------
 
-#define NUM_CH  4
-#define DOUT    0
-#define SCK     1
+#define NUM_CH 4
+#define DOUT 0
+#define SCK 1
 
 int hx711_pin[NUM_CH][2] = {
-    {CH1_DATA, CH1_SCK},
-    {CH2_DATA, CH2_SCK},
-    {CH3_DATA, CH3_SCK},
-    {CH4_DATA, CH4_SCK}};
+  { CH1_DATA, CH1_SCK },
+  { CH2_DATA, CH2_SCK },
+  { CH3_DATA, CH3_SCK },
+  { CH4_DATA, CH4_SCK }
+};
 
 enum HX711_State {
   HX711_RESET = 0,
@@ -65,15 +67,16 @@ enum State {
 
 enum State state;
 
-int32_t raw[NUM_CH] = {0};     //計測結果 生データ
-int32_t offset[NUM_CH] = {0};  //オフセット
-uint32_t read[NUM_CH] = {0};   //読み出し完了フラグ
+int32_t raw[NUM_CH] = { 0 };     //計測結果 生データ
+int32_t offset[NUM_CH] = { 0 };  //オフセット
+uint32_t read[NUM_CH] = { 0 };   //読み出し完了フラグ
+uint32_t counter = 0;
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  for (int i = 0; i < NUM_CH;i++){
+  for (int i = 0; i < NUM_CH; i++) {
     pinMode(hx711_pin[i][DOUT], INPUT);
     pinMode(hx711_pin[i][SCK], OUTPUT);
   }
@@ -83,6 +86,7 @@ void setup() {
   pinMode(LED3, OUTPUT);
   pinMode(LED4, OUTPUT);
   pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_TX, OUTPUT);
 
   pinMode(SW_A, INPUT_PULLUP);
   pinMode(SW_B, INPUT_PULLUP);
@@ -107,22 +111,33 @@ void loop() {
 
   switch (state) {
     case READ:
-      digitalWrite(PIN_LED, HIGH);
+
       if ((read[0] != 0) & (read[1] != 0) & (read[2] != 0) & (read[3] != 0)) {
         for (int i = 0; i < NUM_CH; i++) {
           data[i] = raw[i] - offset[i];
           gram[i] = (double)data[i] * HX711_ADC1bit / HX711_SCALE;
         }
 
-        Serial.printf("%+8d,%+8d,%+8d,%+8d,  %+12.5f,%+12.5f,%+12.5f,%+12.5f,  %d,%d,%d,%d\n",
+        digitalWrite(LED1, read[0] == 1);
+        digitalWrite(LED2, read[1] == 1);
+        digitalWrite(LED3, read[2] == 1);
+        digitalWrite(LED4, read[3] == 1);
+
+        uint8_t tx_state = (counter % 100) < 50 ? 1 : 0;
+        digitalWrite(PIN_TX, tx_state);
+        digitalWrite(PIN_LED, tx_state);
+
+        Serial.printf("%+8d,%+8d,%+8d,%+8d,  %+12.5f,%+12.5f,%+12.5f,%+12.5f,  %d,%d,%d,%d, %c\n",
                       data[0], data[1], data[2], data[3],
                       gram[0], gram[1], gram[2], gram[3],
-                      read[0], read[1], read[2], read[3]);
+                      read[0], read[1], read[2], read[3],
+                      tx_state == 1 ? 'H' : 'L');
 
         read[0] = 0;
         read[1] = 0;
         read[2] = 0;
         read[3] = 0;
+        counter++;
       }
 
       if (digitalRead(SW_A) == LOW) {
@@ -132,6 +147,10 @@ void loop() {
 
     case SET_OFFSET:
       digitalWrite(PIN_LED, LOW);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, LOW);
       Serial.println("SET_OFFSET");
       for (int ch = 0; ch < NUM_CH; ch++) {
         offset[ch] = 0;
@@ -164,7 +183,7 @@ void setup1() {
 
   //core 0の初期化処理待ち
   delay(2000);
-  
+
 
   // HX711 初期化
   // SCKをLOWにして計測を開始する
